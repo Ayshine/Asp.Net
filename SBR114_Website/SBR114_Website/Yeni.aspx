8898c293f6c4ca8a5bb19fcc2ed9481abbb51bad
Bilgi Kozyatağı <bilgikozyatagipclab@outlook.com>
2019-04-12 15:30:09 +0300

    web controls

---
+++ SBR114_Website/SBR114_Website/Yeni.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Yeni.aspx.cs" Inherits="SBR114_Website.Yeni" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Başlık:
+            <asp:TextBox ID="txtBaslik" runat="server" Width="244px" AutoPostBack="True"></asp:TextBox>
+&nbsp;<br />
+            <br />
+            İçerik:
+            <asp:TextBox ID="txtIcerik" runat="server" Height="110px" TextMode="MultiLine" Width="252px" AutoPostBack="True"></asp:TextBox>
+            <br />
+            <br />
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+            <asp:Button ID="btnBold" runat="server" Text="B" OnClick="btnBold_Click" />
+            <asp:Button ID="btnItalic" runat="server" Text="I" OnClick="btnItalic_Click" />
+            <asp:Button ID="btnUnderline" runat="server" Text="U" OnClick="btnUnderline_Click" />
+            <asp:ListBox ID="lstStyle" runat="server" Width="155px" AutoPostBack="True" OnSelectedIndexChanged="lstStyle_SelectedIndexChanged">
+                <asp:ListItem>Times New Roman</asp:ListItem>
+                <asp:ListItem>Arial</asp:ListItem>
+                <asp:ListItem>Tahoma</asp:ListItem>
+            </asp:ListBox>
+            <br />
+            <br />
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+                <asp:ListItem>Baslik</asp:ListItem>
+                <asp:ListItem>Icerik</asp:ListItem>
+            </asp:RadioButtonList>
+            <asp:Label ID="lblBaslik" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:Label ID="lblIcerik" runat="server" Text="Label"></asp:Label>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ SBR114_Website/SBR114_Website/Yeni.aspx.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SBR114_Website
+{
+    public partial class Yeni : System.Web.UI.Page
+    {
+        string selectedControl = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                for (int i = 8; i <= 72; i += 2)
+                {
+                    DropDownList1.Items.Add(i.ToString());
+                }
+            }
+        }
+
+        protected void btnBold_Click(object sender, EventArgs e)
+        {
+
+            lblBaslik.Text = txtBaslik.Text;
+            lblBaslik.Font.Size = Convert.ToInt32(DropDownList1.SelectedValue);
+
+            if (lblBaslik.Font.Bold == true)
+                lblBaslik.Font.Bold = false;
+            else
+                lblBaslik.Font.Bold = true;
+
+            lblIcerik.Text = txtIcerik.Text;
+            lblIcerik.Font.Size = Convert.ToInt32(DropDownList1.SelectedValue);
+
+            if (lblIcerik.Font.Bold == true)
+                lblIcerik.Font.Bold = false;
+            else
+                lblIcerik.Font.Bold = true;
+
+
+        }
+
+        protected void btnItalic_Click(object sender, EventArgs e)
+        {
+            lblBaslik.Text = txtIcerik.Text;
+            lblBaslik.Font.Size = Convert.ToInt32(DropDownList1.SelectedValue);
+
+            if (lblBaslik.Font.Italic == true)
+                lblBaslik.Font.Italic = false;
+            else
+                lblBaslik.Font.Italic = true;
+
+            lblIcerik.Text = txtIcerik.Text;
+            lblIcerik.Font.Size = Convert.ToInt32(DropDownList1.SelectedValue);
+
+            if (lblIcerik.Font.Italic == true)
+                lblIcerik.Font.Italic = false;
+            else
+                lblIcerik.Font.Italic = true;
+
+
+        }
+
+        protected void btnUnderline_Click(object sender, EventArgs e)
+        {
+
+            lblBaslik.Text = txtIcerik.Text;
+            lblBaslik.Font.Size = Convert.ToInt32(DropDownList1.SelectedValue);
+
+            if (lblBaslik.Font.Underline == true)
+                lblBaslik.Font.Underline = false;
+            else
+                lblBaslik.Font.Underline = true;
+
+            lblIcerik.Text = txtIcerik.Text;
+            lblIcerik.Font.Size = Convert.ToInt32(DropDownList1.SelectedValue);
+
+            if (lblIcerik.Font.Underline == true)
+                lblIcerik.Font.Underline = false;
+            else
+                lblIcerik.Font.Underline = true;
+
+
+        }
+
+        protected void lstStyle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            lblBaslik.Font.Name = lstStyle.SelectedItem.Value;
+
+            lblIcerik.Font.Name = lstStyle.SelectedItem.Value;
+
+
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedControl = RadioButtonList1.SelectedItem.ToString();
+            Label1.Text = selectedControl;
+        }
+    }
+}
